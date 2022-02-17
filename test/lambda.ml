6f9422ff2c092747596d5f5dcf7cbc8731bce9c8
zerolfx <zerolfx0@gmail.com>
2022-02-17 03:29:37 -0500

    test: add tests

---
+++ test/lambda.ml
@@ -0,0 +1,39 @@
+open OUnit2
+
+let code_to_int s = 
+  let code = Lix.Parser.code_eof Lix.Lexer.token (Lexing.from_string s) in
+  let ast = Lix.Pipeline.code_to_last code in
+  match Lix.Jit.codegen_repl ast with
+  | _, Some (Lix.Ast.Int i) -> i
+  | _ -> raise (Failure "result is not an int")
+
+let assert_eval_result i s = assert_equal (code_to_int s) i
+
+
+let tests = [
+  "simple_add">::(fun _ -> 
+    assert_eval_result
+    2
+    "(+ 1 1)"
+  );
+  "partial_application">::(fun _ -> 
+    assert_eval_result
+    5
+    "((+ 2) 3)"
+  );
+  "lambda_desugar">::(fun _ -> 
+    assert_eval_result
+    9
+    "((: (-> Int Int Int) (fn (a b) (+ a b))) 4 5)"
+  );
+  "partial_lambda">::(fun _ -> assert_eval_result
+    6
+    "((: (-> (-> Int Int) Int Int) (fn (f a) (f a))) (+ 2) 4)"
+  );
+  "lambda_return_lambda">::(fun _ -> assert_eval_result
+    5
+    "(((: (-> Int (-> Int Int)) 
+          (fn x (: (-> Int Int)
+                   (fn y (+ x y))))) 2) 3)"
+  );
+]
--- test/test.ml
+++ test/test.ml
@@ -11,27 +11,7 @@ let assert_eval_result i s = assert_equal (code_to_int s) i
 
 
 let suite = 
-  "lambda">:::[
-    "simple_add">::(fun _ -> 
-      assert_eval_result
-      2
-      "(+ 1 1)"
-    );
-    "partial_application">::(fun _ -> 
-      assert_eval_result
-      5
-      "((+ 2) 3)"
-    );
-    "lambda_desugar">::(fun _ -> 
-      assert_eval_result
-      9
-      "((: (-> Int Int Int) (fn (a b) (+ a b))) 4 5)"
-    );
-    "partial_lambda">::(fun _ -> assert_eval_result
-      6
-      "((: (-> (-> Int Int) Int Int) (fn (f a) (f a))) (+ 2) 4)"
-    );
-  ]
+  "lambda">:::Lambda.tests
 
 
 
